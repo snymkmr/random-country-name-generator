62170b98a0a5a0a3ef71a3776d551bd38d8639cc
Sañyam Kumar <sanyamkvvs@hotmail.com>
2020-10-15 20:50:49 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import countryList from '../util/countries';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  countries = '';
+  limit = 5;
+
+  handleSlideChange(newLimit: number){
+    this.limit = newLimit;
+  }
+
+  generate(){
+    this.shuffleArray(countryList);
+
+    //India should be a part of random country.
+    let India = countryList.indexOf('India');
+    let rndm = ~~(Math.random() * this.limit) + 0;
+    const tmp = countryList[India]
+    countryList[India] = countryList[rndm]
+    countryList[rndm] = tmp
+    
+    this.countries = countryList.slice(0, this.limit).join(' • ');
+  }
+
+  shuffleArray(array) {
+    for (var i = array.length -1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+  }
+
 }
---
+++ src/util/countries.ts
@@ -0,0 +1,23 @@
+// some random words
+export default [
+ 'India',
+ 'United States',
+ 'China',
+ 'Japan',
+ 'Germany',
+ 'United Kingdom',
+ 'France',
+ 'Italy',
+ 'Brazil',
+ 'Canada',
+ 'Russia',
+ 'South Korea',
+ 'Spain',
+ 'Australia',
+ 'Mexico',
+ 'Indonesia',
+ 'Netherlands',
+ 'Saudi Arabia',
+ 'Turkey',
+ 'Switzerland'
+];
